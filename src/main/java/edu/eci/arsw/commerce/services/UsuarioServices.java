b224467d05db24595e5b8545f13ba573ceba96d2
Camilo Lopez <camilino123@gmail.com>
2018-11-24 17:11:01 -0500

    Model, servicios, persistencia y controlador de usuario implementado, configuracion conexion base de datos añadida

--- src/main/java/edu/eci/arsw/commerce/services/UsuarioServices.java
+++ src/main/java/edu/eci/arsw/commerce/services/UsuarioServices.java
@@ -5,10 +5,50 @@
  */
 package edu.eci.arsw.commerce.services;
 
+import edu.eci.arsw.commerce.model.Usuario;
+import java.util.List;
+
 /**
+ * Cada uno de los serivicios que se ofrecen para los usuarios
  *
  * @author camilo
  */
 public interface UsuarioServices {
-    
+
+    /**
+     * Este metodo permite la creacion de un nuevo usuarios
+     *
+     * @param cedulaUsuario Cedula del usuario
+     * @param nombreUsuario Nombre del usuario
+     * @param apellidoUsuario Apellido del usuario
+     * @param direccionUsuario Direccion del usuario
+     * @param telefonoUsuario Telefono del usuario
+     * @param fechaNacimiento Fecha de nacimiento del usuario
+     */
+    public void crearUnNuevoUsuario(Integer cedulaUsuario, String nombreUsuario,
+            String apellidoUsuario, String direccionUsuario, Integer telefonoUsuario,
+            String fechaNacimiento);
+
+    /**
+     * Este metodo permite obtener un usuario por su cedula
+     *
+     * @param cedula La cedula del usuario a obtener
+     * @return El usuario que pertenece a esa cedula
+     */
+    public Usuario obtenerUsuarioPorCedula(Integer cedula);
+
+    /**
+     * Este metodo permite obtener todos los usuarios
+     *
+     * @return Una lista con todos los usuarios
+     */
+    public List<Usuario> obtenerTodosLosUsuarios();
+
+    /**
+     * Este metodo permite eliminar un usuario por su cedula
+     *
+     * @param cedulaUsuario
+     */
+    public void eliminarUsuarioPorCedula(Integer cedulaUsuario);
+
 }
