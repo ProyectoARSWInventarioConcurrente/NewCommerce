10ee33a7143dabf5f8365c272881d3c1c0613d50
Camilo Lopez <camilino123@gmail.com>
2018-12-04 19:13:54 -0500

    Adicion script axios, creacion controladores para inicio de sesion y registro de usuario

--- src/main/resources/static/js/RegistroUsuario.js
+++ src/main/resources/static/js/RegistroUsuario.js
@@ -4,10 +4,4 @@
  * and open the template in the editor.
  */
 
-/**
- * Esta funcion añade cada uno de los productos que existen para la venta a la
- *  tabla inicial de compra para un usuario
- */
-function cargarProductos(){
-    
-}
+
