8c32dc9859631d0d50a2111353417da4b12358b0
Camilo Lopez <camilino123@gmail.com>
2018-12-11 01:32:31 -0500

    Correccion obtencion variedad por id

--- src/main/java/edu/eci/arsw/commerce/controllers/ProductAPIController.java
+++ src/main/java/edu/eci/arsw/commerce/controllers/ProductAPIController.java
@@ -179,7 +179,7 @@ public class ProductAPIController {
      * @param idVariedad El id de la variedad que se quiere retornar.
      * @return El estado de la peticion HTTP
      */
-    @RequestMapping(method = RequestMethod.GET, path = {"variedades/{idVariedad}"})
+    @RequestMapping(method = RequestMethod.GET, path = {"variedad/{idVariedad}"})
     public ResponseEntity<?> obtenerVariedadPorId(@PathVariable("idVariedad") String idVariedad) {
         try {
             Map<String, VariedadProducto> variedades = new HashMap();
